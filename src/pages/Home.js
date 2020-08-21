c38e395c133458d16960b4c0e478ed8285d88bbb
Cosmin-Stefan Dobranici <cosmin.dobranici16@bathspa.ac.uk>
2020-08-22 00:32:18 +0300

    Added sections to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,9 @@ import { Container, Row, Col } from "react-bootstrap";
 import { homePageContent } from "../repository/content";
 import LineBreak from "../components/LineBreak";
 import ReservationForm from "../components/ReservationForm";
+import { AiOutlineFieldTime } from "react-icons/ai";
+import { GoLocation } from "react-icons/go";
+import { GiBottleVapors } from "react-icons/gi";
 
 export const Home = () => {
   return (
@@ -11,17 +14,150 @@ export const Home = () => {
       <Carousel />
       <Container fluid="md" className="home-header">
         <Row>
-          <Col md={12} lg={6}>
+          <Col md={12} lg={7}>
             <Row>
-              <h1>{homePageContent.mainHeaderTitle}</h1>
-              <LineBreak />
+              <Col>
+                <h1>{homePageContent.mainHeaderTitle}</h1>
+                <LineBreak />
+              </Col>
             </Row>
             <Row>
-              <p>{homePageContent.mainHeaderDescription}</p>
+              <Col xs={7}>
+                <p>{homePageContent.mainHeaderDescription}</p>
+              </Col>
+              <Col xs={5}>
+                <Row>
+                  <Col>
+                    <h3>
+                      <AiOutlineFieldTime style={{ color: "#af9a7d" }} />{" "}
+                      Opening Time:
+                    </h3>
+                    <h6>Monday - Thursday </h6>
+                    <p>6PM - 12AM</p>
+                    <h6>Friday</h6>
+                    <p>6PM - 1AM</p>
+                    <h6>Saturday</h6>
+                    <p>12PM - 2AM</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <h3>
+                      <GoLocation style={{ color: "#af9a7d" }} /> Located:
+                    </h3>
+                    <h6>10 Manvers St Bath BA1 1JQ</h6>
+                  </Col>
+                </Row>
+              </Col>
             </Row>
           </Col>
-          <Col md={12} lg={6}>
+          <Col md={12} lg={5}>
             <ReservationForm />
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid className="home-shisha-section">
+        <Row className="justify-content-center">
+          <Col xs={6} className="home-shisha-section-col">
+            <h1>{homePageContent.shishaSectionTitle}</h1>
+            <div className="vertical-line-banner"></div>
+            <p>{homePageContent.shishaSectionDescription}</p>
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid className="home-prices-section">
+        <Row className="justify-content-center">
+          <Col xs={6} className="home-prices-section-col">
+            <h1>{homePageContent.pricesSectionTitle}</h1>
+            <div className="vertical-line-banner"></div>
+            <p>{homePageContent.pricesSectionDescription}</p>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col xs={3} lg={2} className="home-prices-section-col-drinks">
+            <h3>Cocktails</h3>
+            <h1>2 for £12</h1>
+            <h6>1 for £7</h6>
+          </Col>
+          <Col xs={3} lg={2} className="home-prices-section-col-drinks">
+            <h3>Shooters</h3>
+            <h1>5 for £12</h1>
+            <h6>1 for £3</h6>
+          </Col>
+          <Col xs={3} lg={2} className="home-prices-section-col-drinks">
+            <h3>Hookah</h3>
+            <h1>2 Flavours for £14</h1>
+            <h6>More flavours at an extra cost</h6>
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid className="home-menu-section">
+        <Row className="justify-content-center">
+          <Col xs={8} className="home-menu-section-col">
+            <h1>Shisha Flavours And Cocktails</h1>
+            <div className="vertical-line-banner"></div>
+            <Row>
+              <h3 style={{ "margin-left": "9vw" }}>
+                <GiBottleVapors style={{ color: "#af9a7d" }} /> Shisha Flavours
+              </h3>
+            </Row>
+            <Row className="justify-content-center">
+              <Col xs={12} md={3}>
+                <ul className="menu-list">
+                  <li className="menu-list-item">Cali Lime</li>
+                  <li className="menu-list-item">Froz Lemon</li>
+                  <li className="menu-list-item">Froz Apple</li>
+                  <li className="menu-list-item">Froz Raspberry</li>
+                  <li className="menu-list-item">Mango</li>
+                </ul>
+              </Col>
+              <Col xs={12} md={3}>
+                <ul className="menu-list">
+                  <li className="menu-list-item">Passion Fruit</li>
+                  <li className="menu-list-item">Mint</li>
+                  <li className="menu-list-item">Blue Mist</li>
+                  <li className="menu-list-item">Strawberry</li>
+                  <li className="menu-list-item">Bubblegum</li>
+                </ul>
+              </Col>
+              <Col xs={12} md={3}>
+                <ul className="menu-list">
+                  <li className="menu-list-item">Watermelon</li>
+                  <li className="menu-list-item">Grape</li>
+                </ul>
+              </Col>
+            </Row>
+            <Row>
+              <h3 style={{ "margin-left": "9vw", "margin-top": "" }}>
+                <GiBottleVapors style={{ color: "#af9a7d" }} /> Shisha Flavours
+              </h3>
+            </Row>
+            <Row className="justify-content-center">
+              <Col xs={12} md={3}>
+                <ul className="menu-list">
+                  <li className="menu-list-item">Cali Lime</li>
+                  <li className="menu-list-item">Froz Lemon</li>
+                  <li className="menu-list-item">Froz Apple</li>
+                  <li className="menu-list-item">Froz Raspberry</li>
+                  <li className="menu-list-item">Mango</li>
+                </ul>
+              </Col>
+              <Col xs={12} md={3}>
+                <ul className="menu-list">
+                  <li className="menu-list-item">Passion Fruit</li>
+                  <li className="menu-list-item">Mint</li>
+                  <li className="menu-list-item">Blue Mist</li>
+                  <li className="menu-list-item">Strawberry</li>
+                  <li className="menu-list-item">Bubblegum</li>
+                </ul>
+              </Col>
+              <Col xs={12} md={3}>
+                <ul className="menu-list">
+                  <li className="menu-list-item">Watermelon</li>
+                  <li className="menu-list-item">Grape</li>
+                </ul>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Container>
--- src/repository/content.js
+++ src/repository/content.js
@@ -2,4 +2,16 @@ export const homePageContent = {
   mainHeaderTitle: "Welcome to The Earl and Terrace Cocktail and Shisha Bars",
   mainHeaderDescription:
     "Whether you need to relax and wind down after a hard day’s work or want to celebrate the weekend in style, The Earl has you covered.\n\n\nStart your evening by kicking back on our heated terrace, sipping one of our finest cocktails or enjoying a cold draught beer with your mates. With the best shisha in the city, two terrace bars, board games and giant outdoor games we provide a truly relaxed atmosphere in the heart of Bath. Inside the main bar, we also host all the biggest sporting events with a number of large 4K TVs.\n\n\nAs the sun sets and night draws in we turn up the energy at The Earl, with regular DJ’s playing the biggest tunes on weekend evenings, located by our central dance floor. We also offer VIP experiences, private hire and cocktail masterclasses for those that want to make their night out one to remember.",
+  shishaSectionTitle: "Come on in and get your Shisha!",
+  shishaSectionDescription:
+    "Our customers can enjoy a nice drink with their shisha. With nearly 100 drinks and multiple shisha flavours choose from, our customers never run out of options.",
+  pricesSectionTitle: "Offers & Prices",
+  pricesSectionDescription:
+    "At the Earl and Terrace, we focus on giving you the best quality for the best price. Our drinks have specific prices but we have offers for buying two drinks or multiple shots/shooters. All of our hookahs include free mouth tips and ice tray. We also provide natural coals upon request.",
+  gallerySectionTitle: "Photo Gallery",
+  gallerySectionDescription:
+    "A relaxing environment offering flavorful iced hookahs, rich espresso and coffee, delicious teas, chill music, a fun and friendly staff and plenty of comfy couches to relax in. Smoke. Drink. Talk. Study. Lounge with us.",
+  groupHangoutSectionTitle: "Perfect setting for a group hangout",
+  groupHangoutSectionDescription:
+    "Our welcoming atmosphere & delicious Shisha options create the perfect setting for a group hangout. Whether you are planning a night out with your group or want to cool off from a stressful day, we have you covered.",
 };
